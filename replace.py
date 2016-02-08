c7575ebf1fba7dda8f649bfb6a4b72df91415c7f
tripod31 <tripod31@hotmail.com>
2016-02-08 13:35:59 +0900

    1st

---
+++ replace.py
@@ -0,0 +1,86 @@
+# coding:utf-8
+'''
+replace string in files in specified directory.
+'''
+import argparse
+#import re
+import os
+import sys
+
+from yoshi.util import find_all_files,is_match_patterns_fnmatch,get_encoding,EncodeException    #@UnresolvedImport
+
+def preview(file,from_str):
+    hit=False
+    try:
+        enc,data = get_encoding(file)
+        if data.find(from_str) != -1:
+            hit = True
+    except Exception as e:
+        sys.stderr.write(e)
+        return
+    finally:
+        pass
+            
+    if hit:
+        print("hit:%s" % (file))
+
+def replace(file,from_str,to_str):
+    hit =False
+    try:
+        enc,data = get_encoding(file)
+        temp_file= file+".tmp"
+        if data.find(from_str) != -1:
+            hit = True
+        
+            data = data.replace(from_str, to_str)
+            with open(temp_file, 'w',encoding=enc,newline='') as f:    #newline='':leave end of line chars as they are
+                f.write(data)
+                f.close()
+    except Exception as e:
+        sys.stderr.write(e)
+        return
+    
+    if not hit:
+        return
+    
+    #verify data
+    with open(temp_file, 'r',encoding=enc,newline='') as f:
+        data_new = f.read()
+    
+    if data_new == data:
+        os.remove(file)
+        os.rename(temp_file, file)
+    else:
+        raise EncodeException('encoding failed')
+        
+    print("replaced:%s" % (file))
+
+def process(start_dir,file_pattern,from_str,to_str,p_preview):
+    files = find_all_files(start_dir)
+    for file in files:
+        if is_match_patterns_fnmatch(file, file_pattern.split()):
+            if p_preview:
+                preview(file,from_str)
+            else:
+                replace(file,from_str,to_str)
+    
+    print("finished.")
+
+if __name__ == '__main__':
+    
+    #arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--start_dir',
+                        default=".")
+    parser.add_argument('--file_pattern',
+                        help="pattern of the name of file to be replaced,in wild card format.ex)'*.txt,*.bat'")
+    parser.add_argument('--from_str')
+    parser.add_argument('--to_str')
+    parser.add_argument('--preview',
+                        action='store_true',default=False,
+                        help="do not change files when specified")
+    args=parser.parse_args()
+    #print(args)
+    process(args.start_dir,args.file_pattern,args.from_str,args.to_str,args.preview)
+            
+    sys.exit(0)
---
+++ test.py
@@ -0,0 +1,69 @@
+# coding:utf-8
+import unittest
+import subprocess
+import os
+from replace import process #@UnresolvedImport
+
+def create_file():
+    if not os.path.exists('test'):
+        os.mkdir('test')
+    
+    with open("test/utf8.txt","w",encoding="utf-8") as f:
+        f.write("あああ\nabc変更前def\nいいい\n")
+        f.close()
+    with open("test/cp932.txt","w",encoding="cp932") as f:
+        f.write("あああ\nabc変更前def\nいいい\n")
+        f.close()
+
+def byte2str(bytes):
+    return bytes.decode("utf-8").replace("\r\n","\n")
+
+def exec_command(cmd):
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.wait()
+    stdout_data, stderr_data = p.communicate()
+    return p.returncode,byte2str(stdout_data),byte2str(stderr_data)
+
+class Test1(unittest.TestCase):
+    def setUp(self):
+        create_file()
+    
+    def test_call(self):
+        process('test','*.txt','変更前','変えたよ',False)
+        with open("test/utf8.txt","r",encoding="utf-8") as f:
+            data=f.read()
+            f.close()
+        self.assertEqual(data,"あああ\nabc変えたよdef\nいいい\n")         
+    
+    def test_exec(self):
+        cmd = "python replace.py --start_dir test --file_pattern *.txt --from_str 変更前 --to_str 変えたよ"
+        ret,stdout,stderr = exec_command(cmd)
+        print( stderr+stdout)        
+        self.assertEqual(ret,0)
+        self.assertEqual(len(stderr), 0)
+                #read file
+        with open("test/utf8.txt","r",encoding="utf-8") as f:
+            data=f.read()
+            f.close()
+        self.assertEqual(data,"あああ\nabc変えたよdef\nいいい\n") 
+    
+    def test_preview(self):
+        cmd = "python replace.py --start_dir test --file_pattern *.txt --from_str 変更前 --to_str 変えたよ --preview"
+        ret,stdout,stderr = exec_command(cmd)
+        print( stderr+stdout)        
+        self.assertEqual(ret,0)
+        self.assertEqual(len(stderr), 0)
+        #read file
+        with open("test/utf8.txt","r",encoding="utf-8") as f:
+            data=f.read()
+            f.close()
+        self.assertEqual(data,"あああ\nabc変更前def\nいいい\n") 
+        
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    #unittest.main()
+    suite = unittest.TestSuite()
+    suite.addTest(Test1('test_call'))
+    unittest.TextTestRunner(verbosity=2).run(suite)
